024de4f96024d347bc99019558588763000f1d03
David A Roberts <d@vidr.cc>
2024-10-11 11:06:13 +0000

    Store verified programs

--- test.sh
+++ test.sh
@@ -4,6 +4,11 @@
 passes=0
 total=0
 
+verified() {
+    mkdir -p "verified/$(dirname "$1")"
+    cp -f "$1" "verified/$1"
+}
+
 # Function to test a single Python script
 test_script() {
     python_file="$1"
@@ -16,17 +21,44 @@ test_script() {
 
     # Run the Python script and compare with JSON
     output="$(timeout 10 python "$python_file")"
-    if echo "$output" | ./diff.py "$json_file" > /dev/null 2>&1; then
+    output="${output%, }"
+    output="${output%,}"
+    if echo "$output" | diff -Z "$json_file" - > /dev/null 2>&1; then
         echo "PASS ($passes / $total)"
         ((passes++))
-    elif echo "[${output%, }]" | ./diff.py "$json_file" > /dev/null 2>&1; then
+        verified "$python_file"
+    elif echo "$output" | ./diff.py "$json_file" > /dev/null 2>&1; then
+        echo "PREFIX ($passes / $total)"
+        ((passes++))
+        verified "$python_file"
+    elif echo "[$output]" | ./diff.py "$json_file" > /dev/null 2>&1; then
         echo MALFORMED
         ((passes++))
     elif [ -z "$output" ]; then
-        output="$((cat "$python_file"; echo; echo "print([$seqname(n) for n in range(30)])") | timeout 10 python 2>/dev/null)"
-        if echo "$output" | ./diff.py "$json_file" > /dev/null 2>&1; then
+        if (cat "$python_file"; echo; echo "print([$seqname(n) for n in range(30)])") | timeout 3 python | ./diff.py "$json_file" > /dev/null 2>&1; then
             echo DEF
             ((passes++))
+            echo >> "$python_file"
+            echo "print([$seqname(n) for n in range(30)])" >> "$python_file"
+            verified "$python_file"
+        elif (cat "$python_file"; echo; echo "print([a(n) for n in range(30)])") | timeout 3 python 2>/dev/null | ./diff.py "$json_file" > /dev/null 2>&1; then
+            echo DEFA
+            ((passes++))
+            echo >> "$python_file"
+            echo "print([a(n) for n in range(30)])" >> "$python_file"
+            verified "$python_file"
+        elif (cat "$python_file"; echo; echo "print(${seqname}_list)") | timeout 3 python 2>/dev/null | ./diff.py "$json_file" > /dev/null 2>&1; then
+            echo LIST
+            ((passes++))
+            echo >> "$python_file"
+            echo "print(${seqname}_list)" >> "$python_file"
+            verified "$python_file"
+        elif (cat "$python_file"; echo; echo "print(${seqname})") | timeout 3 python 2>/dev/null | ./diff.py "$json_file" > /dev/null 2>&1; then
+            echo LIST
+            ((passes++))
+            echo >> "$python_file"
+            echo "print(${seqname})" >> "$python_file"
+            verified "$python_file"
         else
             echo EMPTY
         fi
